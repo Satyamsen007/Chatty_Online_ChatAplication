cb5657a926c4d8d59a7e4a1421a57423db79ebbf
Satyamsen007 <satyamsen280@gmail.com>
2025-06-12 15:01:26 +0530

    some changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5003;
 
 // CORS Configuration
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, ' http://localhost:5173/'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept", "Origin", "X-Requested-With"],
